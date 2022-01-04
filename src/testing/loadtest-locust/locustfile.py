7f1e6d7302e447a97d236aeeb7c0c44d11a1a8a8
Sebastian Bader <sebader@microsoft.com>
2022-01-04 14:00:50 +0100

    fix locustfile

--- src/testing/loadtest-locust/locustfile.py
+++ src/testing/loadtest-locust/locustfile.py
@@ -16,7 +16,7 @@ class WebsiteUserSequence(SequentialTaskSet):
     @task(2)
     def list_catalogitems(self):
         # Get list of catatlog items
-        with self.client.get("/api/1.0/catalogitem", name="GET List of CatalogItems", headers=self.headers, catch_response=True) as response:
+        with self.client.get("/api/1.0/catalogitem", name="GET List of CatalogItems", catch_response=True) as response:
             if response.status_code != 200:
                 logging.error(f"(list_catalogitems) - failed response. Code: {response.status_code}")
                 response.failure(f"Got wrong response. Code: {response.status_code}")
@@ -25,15 +25,23 @@ class WebsiteUserSequence(SequentialTaskSet):
                 # Pick a random item from the catalog for further actions
                 self.randomItemId = random.choice(json_response)['id']
 
+    @task(20)
+    def show_catalogitem(self):
+        # Open a catalog item
+        with self.client.get(f"/api/1.0/catalogitem/{self.randomItemId}", name="GET Catalog Item", catch_response=True) as response:
+            if response.status_code != 200:
+                logging.error(f"(show_catalogitem) - failed response. Code: {response.status_code}")
+                response.failure(f"Got wrong response. Code: {response.status_code}")
+
     @task(10)
     def post_new_rating(self):
         # Post a new rating
         json_body = {
             "rating": random.randint(1, 5)
         }
-        with self.client.post(f"/api/1.0/catalogitem/{self.randomItemId}/ratings", json=json_body, name="POST new rating", headers=self.headers, catch_response=True) as response:
+        with self.client.post(f"/api/1.0/catalogitem/{self.randomItemId}/ratings", json=json_body, name="POST new rating", catch_response=True) as response:
             if response.status_code != 202:
-                logging.error(f"(post_new_comment) - failed response. Code: {response.status_code}")
+                logging.error(f"(post_new_rating) - failed response. Code: {response.status_code}")
                 response.failure(f"Got wrong response. Code: {response.status_code}")
 
     @task(2)
@@ -43,7 +51,7 @@ class WebsiteUserSequence(SequentialTaskSet):
             "authorName": "Locust Test User",
             "text": "This is a load test entry"
         }
-        with self.client.post(f"/api/1.0/catalogitem/{self.randomItemId}/comments", json=json_body, name="POST new comment", headers=self.headers, catch_response=True) as response:
+        with self.client.post(f"/api/1.0/catalogitem/{self.randomItemId}/comments", json=json_body, name="POST new comment", catch_response=True) as response:
             if response.status_code != 202:
                 logging.error(f"(post_new_comment) - failed response. Code: {response.status_code}")
                 response.failure(f"Got wrong response. Code: {response.status_code}")

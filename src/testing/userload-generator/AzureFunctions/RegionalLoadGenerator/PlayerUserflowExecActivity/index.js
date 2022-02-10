b50aed6a549e0aa3c4ae375eb2d5c9ff11420e1a
Sebastian Bader <sebader@microsoft.com>
2022-02-10 15:33:42 +0100

    log errors

--- src/testing/userload-generator/AzureFunctions/RegionalLoadGenerator/PlayerUserflowExecActivity/index.js
+++ src/testing/userload-generator/AzureFunctions/RegionalLoadGenerator/PlayerUserflowExecActivity/index.js
@@ -37,7 +37,7 @@ module.exports = async function (context) {
         let status = 200;
 
         if ((output).error) {
-            context.log("ERROR: " + JSON.stringify((output).error));
+            context.log.error("ERROR: " + JSON.stringify((output).error));
             status = 500;
             responseMessage += "\r\nError running tests: " + JSON.stringify((output).error, null, 2);
         }
@@ -45,7 +45,7 @@ module.exports = async function (context) {
         responseMessage += "\r\nSTDOUT: " + (output).stdout;
         context.log("STDOUT: " + (output).stdout);
         if ((output).stderr) {
-            context.log("STDERR: " + (output).stderr);
+            context.log.error("STDERR: " + (output).stderr);
             responseMessage += "\r\nSTDERR: " + (output).stderr;
         }
 
@@ -54,7 +54,7 @@ module.exports = async function (context) {
             message: responseMessage
         };
     } catch (ex) {
-        context.log("ERROR: Failed to run Playwright tests: " + ex);
+        context.log.error("ERROR: Failed to run Playwright tests: " + ex);
         return {
             status: 500,
             message: ex

cff67a1572e3c06d47d6f8eb671b71c538d5a7ba
heoelri <heoelri@users.noreply.github.com>
2022-02-23 18:17:00 +0100

    Grafana - Adding a default nsg for compliance (#116)
    * add default nsg
    
    * push
    
    * use the right tags
    
    * fix
    
    * fmt
    
    * bugfix
    
    * push debug test
    
    * drop debug name
    
    * bump tf to 2.97.0
    
    Co-authored-by: Niels Buit <buit@outlook.com>

--- src/infra/monitoring/grafana/terraform/stamps/network.tf
+++ src/infra/monitoring/grafana/terraform/stamps/network.tf
@@ -44,6 +44,13 @@ resource "azurerm_subnet" "snet_app_outbound" {
   }
 }
 
+# NSG - Assign default nsg to snet_app_outbound subnet
+resource "azurerm_subnet_network_security_group_association" "snet_app_outbound_nsg" {
+  for_each                  = var.stamps
+  subnet_id                 = azurerm_subnet.snet_app_outbound[each.key].id
+  network_security_group_id = azurerm_network_security_group.default[each.key].id
+}
+
 # Dedicated subnet for all backend datastores including PGDB.
 resource "azurerm_subnet" "snet_datastores" {
   for_each                                       = var.stamps
@@ -52,4 +59,25 @@ resource "azurerm_subnet" "snet_datastores" {
   virtual_network_name                           = azurerm_virtual_network.vnet[each.key].name
   resource_group_name                            = azurerm_resource_group.rg[each.key].name
   enforce_private_link_endpoint_network_policies = true
+}
+
+# NSG - Assign default nsg to snet_datastores subnet
+resource "azurerm_subnet_network_security_group_association" "snet_datastores_nsg" {
+  for_each                  = var.stamps
+  subnet_id                 = azurerm_subnet.snet_datastores[each.key].id
+  network_security_group_id = azurerm_network_security_group.default[each.key].id
+}
+
+# Default Network Security Group (nsg) definition
+# Allows outbound and intra-vnet/cross-subnet communication
+resource "azurerm_network_security_group" "default" {
+  for_each            = var.stamps
+  name                = "${local.prefix}-${substr(each.value["location"], 0, 5)}-nsg"
+  location            = azurerm_resource_group.rg[each.key].location
+  resource_group_name = azurerm_resource_group.rg[each.key].name
+
+  # not specifying any security_rules {} will create Azure's default set of NSG rules
+  # it allows intra-vnet communication and outbound public internet access
+
+  tags = local.default_tags
 }

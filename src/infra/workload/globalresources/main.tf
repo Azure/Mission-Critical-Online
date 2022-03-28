e24d396d1b62a2b1db24846b76a29204428acde8
Heyko Oelrichs <heoelri@microsoft.com>
2022-03-28 17:07:47 +0200

    fix type

--- src/infra/workload/globalresources/main.tf
+++ src/infra/workload/globalresources/main.tf
@@ -14,7 +14,7 @@ provider "azurerm" {
 
     # Only in E2E can non-empty resource groups be deleted. This is not allowed in INT and PROD.
     resource_group {
-      prevent_deletion_if_contains_resources = var.environment == "e2e" ? "false" : "true"
+      prevent_deletion_if_contains_resources = var.environment == "e2e" ? false : true
     }
   }
 }

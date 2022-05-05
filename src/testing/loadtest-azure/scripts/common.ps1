c01a1e18eca6945791200c9f84211fb68c116b7b
Heyko Oelrichs <heoelri@microsoft.com>
2022-05-05 08:40:48 +0200

    push

--- src/testing/loadtest-azure/scripts/common.ps1
+++ src/testing/loadtest-azure/scripts/common.ps1
@@ -1,9 +1,6 @@
 $tenantId = az account show -o tsv --query 'tenantId'
 $subscriptionId = az account show -o tsv --query 'id'
 
-$apiVersion = "2021-07-01-preview"
-$apiEndpoint = "https://5e241573-3a5f-4361-bf53-1ae7bde73cb7.neu.cnt-prod.loadtesting.azure.com" #needs to be updated... will be returned when resource is created
-
 $accessToken = az account get-access-token -o tsv --subscription $subscriptionId --query 'accessToken'
 
 $accessTokenHeader = "Authorization=Bearer " + $accessToken

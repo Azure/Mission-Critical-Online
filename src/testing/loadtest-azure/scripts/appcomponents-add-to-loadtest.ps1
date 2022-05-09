7b7b56e2cebbc14149905360c73c5ded85a504e9
Heyko Oelrichs <heoelri@microsoft.com>
2022-05-09 15:05:01 +0200

    update

--- src/testing/loadtest-azure/scripts/appcomponents-add-to-loadtest.ps1
+++ src/testing/loadtest-azure/scripts/appcomponents-add-to-loadtest.ps1
@@ -73,7 +73,6 @@ function AppComponent {
     }
 "@
 
-  Write-Host $result
   return $result
 }
 
--- src/testing/loadtest-azure/scripts/common.ps1
+++ src/testing/loadtest-azure/scripts/common.ps1
@@ -10,7 +10,3 @@ $accessTokenHeader = "Authorization=Bearer " + $accessToken
 # Write access token to file as otherwise HTTP request too long to use az rest in Powershell
 $accessTokenFileName = "./accesstoken.txt"
 $accessTokenHeader | Out-File -FilePath $accessTokenFileName -Force
-
-# The data plane API is using self-signed certificates
-#$env:ADAL_PYTHON_SSL_NO_VERIFY = '1'
-#$env:AZURE_CLI_DISABLE_CONNECTION_VERIFICATION = '1'

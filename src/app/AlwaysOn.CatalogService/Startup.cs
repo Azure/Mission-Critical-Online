e41e0b5cb237bcda561f26de0a2444802eda340f
Sebastian <sebader@microsoft.com>
2022-05-09 13:23:01 +0200

    use AppPathBase and no rewrite

--- src/app/AlwaysOn.CatalogService/Startup.cs
+++ src/app/AlwaysOn.CatalogService/Startup.cs
@@ -89,6 +89,8 @@ namespace AlwaysOn.CatalogService
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UsePathBase("/catalogservice");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
--- src/app/AlwaysOn.HealthService/Startup.cs
+++ src/app/AlwaysOn.HealthService/Startup.cs
@@ -72,6 +72,8 @@ namespace AlwaysOn.HealthService
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UsePathBase("/healthservice");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();

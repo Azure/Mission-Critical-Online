9d4b0bd4e7a8dd3825695a193b2a473c9644383c
Sebastian <sebader@microsoft.com>
2022-05-30 14:05:07 +0200

    optimize

--- src/app/AlwaysOn.CatalogService/Startup.cs
+++ src/app/AlwaysOn.CatalogService/Startup.cs
@@ -1,4 +1,3 @@
-using AlwaysOn.CatalogService.Auth;
 using AlwaysOn.CatalogService.SwaggerHelpers;
 using AlwaysOn.Shared;
 using AlwaysOn.Shared.Interfaces;
@@ -15,12 +14,10 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Primitives;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Reflection;
 using System.Threading.Tasks;
@@ -41,7 +38,7 @@ namespace AlwaysOn.CatalogService
             services.AddSingleton<SysConfiguration>();
 
             services.AddSingleton(typeof(ITelemetryChannel),
-                                new ServerTelemetryChannel() { StorageFolder = "/tmp"});
+                                new ServerTelemetryChannel() { StorageFolder = "/tmp" });
             services.AddApplicationInsightsTelemetry(new ApplicationInsightsServiceOptions()
             {
                 ConnectionString = Configuration[SysConfiguration.ApplicationInsightsConnStringKeyName],
@@ -137,23 +134,24 @@ namespace AlwaysOn.CatalogService
 
             app.Use(async (context, next) =>
             {
-                // Add tracing headers to each response
-                // Source: https://khalidabuhakmeh.com/add-headers-to-a-response-in-aspnet-5
                 context.Response.OnStarting(o =>
                 {
                     if (o is HttpContext ctx)
                     {
                         // In order to get relative location headers (without the host part), we modify any location header here
                         // This is to simplify the reverse-proxy setup in front of the application
-                        IHeaderDictionary headers = context.Response.Headers;
-                        if (headers.TryGetValue("location", out StringValues locationHeaderValue))
+                        try
                         {
-                            var locationUrl = new Uri(locationHeaderValue.FirstOrDefault());
-
-                            context.Response.Headers.Remove("location");
-                            context.Response.Headers.Add("location", locationUrl.PathAndQuery);
+                            if (context.Response.Headers.Location.Count > 0)
+                            {
+                                var locationUrl = new Uri(context.Response.Headers.Location);
+                                context.Response.Headers.Location = locationUrl.PathAndQuery;
+                            }
                         }
+                        catch (Exception) { }
 
+                        // Add tracing headers to each response
+                        // Source: https://khalidabuhakmeh.com/add-headers-to-a-response-in-aspnet-5
                         context.Response.Headers.Add("X-Server-Name", Environment.MachineName);
                         context.Response.Headers.Add("X-Server-Location", sysConfig.AzureRegion);
                         context.Response.Headers.Add("X-Correlation-ID", Activity.Current?.RootId);

61a1d05210f49dd40308c70f50a4a2f8202498ed
Martin Simecek <msimecek@users.noreply.github.com>
2022-06-03 10:29:33 +0200

    Summary comments

--- src/app/AlwaysOn.Shared/Models/AoSqlModelBase.cs
+++ src/app/AlwaysOn.Shared/Models/AoSqlModelBase.cs
@@ -10,8 +10,15 @@ namespace AlwaysOn.Shared.Models
     {
         public int Id { get; set; }
 
+        /// <summary>
+        /// This property determines which item in the database is the latest.
+        /// </summary>
         public DateTime CreationDate { get; set; }
 
+        /// <summary>
+        /// When an item shouldn't be returned from the database and should be scheduled for deletion,
+        /// this property should be set to true.
+        /// </summary>
         public bool Deleted { get; set; }
     }
 }

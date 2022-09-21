4162e4e0f872ed38643250d1b96529441a4f3c24
Heyko Oelrichs <heoelri@microsoft.com>
2022-09-21 13:21:11 +0200

    change sig name

--- src/infra/workload/globalresources/imagegallery.tf
+++ src/infra/workload/globalresources/imagegallery.tf
@@ -1,5 +1,5 @@
 resource "azurerm_shared_image_gallery" "main" {
-  name                = "${local.prefix}-image-gallery"
+  name                = "${local.prefix}_image_gallery"
   location            = azurerm_resource_group.global.location
   resource_group_name = azurerm_resource_group.global.name
   description         = "Shared images and things."

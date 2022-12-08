84635ae477077960cbf7f86f5563065a8431231a
Heyko Oelrichs <heoelri@microsoft.com>
2022-12-08 09:35:39 +0100

    move to central locals block

--- src/infra/workload/releaseunit/modules/stamp/locals.tf
+++ src/infra/workload/releaseunit/modules/stamp/locals.tf
@@ -8,4 +8,15 @@ locals {
   location_short = substr(var.location, 0, 9) # shortened location name used for resource naming
 
   global_resource_prefix = regex("^(.+)-global-rg$", var.global_resource_group_name)[0] # extract global resource prefix from the global resource group name
+
+  # regions where the creation of federated identity credentials is not supported on user-assigned managed identities
+  # https://learn.microsoft.com/azure/active-directory/develop/workload-identity-federation-considerations#unsupported-regions-user-assigned-managed-identities
+  unsupported_regions = [
+    "swedencentral",
+    "swedensouth",
+    "germanynorth",
+    "switzerlandwest",
+    "eastasia",
+    "qatarcentral"
+  ]
 }

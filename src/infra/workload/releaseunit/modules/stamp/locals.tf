fcb1f94799deb83b6de6f144d611f2822b5c9846
heoelri <heoelri@users.noreply.github.com>
2022-12-08 10:23:23 +0100

    Update src/infra/workload/releaseunit/modules/stamp/locals.tf
    Co-authored-by: Martin Simecek <msimecek@users.noreply.github.com>

--- src/infra/workload/releaseunit/modules/stamp/locals.tf
+++ src/infra/workload/releaseunit/modules/stamp/locals.tf
@@ -8,5 +8,4 @@ locals {
   location_short = substr(var.location, 0, 9) # shortened location name used for resource naming
 
   global_resource_prefix = regex("^(.+)-global-rg$", var.global_resource_group_name)[0] # extract global resource prefix from the global resource group name
-
 }

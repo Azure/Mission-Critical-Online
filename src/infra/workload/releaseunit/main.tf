8cd11006d535feb0825745077cf6053e29b7a854
Heyko Oelrichs <heoelri@microsoft.com>
2022-12-22 10:08:04 +0100

    add missing provider blocks

--- src/infra/workload/releaseunit/main.tf
+++ src/infra/workload/releaseunit/main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "3.34.0"
     }
+    azapi = {
+      source  = "Azure/azapi"
+      version = "1.1.0"
+    }
   }
 
   backend "azurerm" {}

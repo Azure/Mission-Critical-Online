1b4ad4ea2e74291290d757ae199f9bcd20028eff
Heyko Oelrichs <heoelri@microsoft.com>
2023-01-11 15:43:04 +0100

    change

--- src/infra/monitoring/grafana/terraform/globalresources/azuread.tf
+++ src/infra/monitoring/grafana/terraform/globalresources/azuread.tf
@@ -12,32 +12,46 @@ resource "azuread_application" "auth" {
   required_resource_access {
     resource_app_id = "00000003-0000-0000-c000-000000000000" # ID of Microsoft Graph
 
+    # add group.readwrite.all permissions
     resource_access {
         id   = "5b567255-7703-4780-807c-7be8301ae99b"
         type = "Role"
     }
   }
 
+  # Define a role for grafana read-only users
   app_role {
     allowed_member_types = ["User"]
     description = "Grafana read only users"
     display_name = "Grafana viewer"
     enabled = "true"
     value = "Viewer"
+    id = random_uuid.app_role_viewer.result
   }
 
+  # Define a role for grafana administrators
   app_role {
     allowed_member_types = ["User"]
     description = "Grafana org admin users"
     display_name = "Grafana admin"
     enabled = "true"
     value = "Admin"
+    id = random_uuid.app_role_admin.result
   }
 
   web {
     redirect_uris = ["https://${var.custom_fqdn != "" ? var.custom_fqdn : azurerm_frontdoor.afdgrafana.cname}/login/azuread"]
 
   }
+}
+
+resource "random_uuid" "app_role_viewer" {
+}
+
+resource "random_uuid" "app_role_admin" {
+}
+
+resource "random_uuid" "test" {
 }
 
 resource "azuread_application_password" "auth" {

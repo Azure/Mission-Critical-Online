053e9df0c2a46c499a855d1e3e6c55c1ac68bb25
Heyko Oelrichs <heoelri@microsoft.com>
2023-03-09 14:36:12 +0100

    bump tf versions

--- src/infra/workload/releaseunit/main.tf
+++ src/infra/workload/releaseunit/main.tf
@@ -6,7 +6,7 @@ terraform {
     }
     azapi = {
       source  = "Azure/azapi"
-      version = "1.2.0"
+      version = "1.4.0"
     }
   }
 
--- src/infra/workload/releaseunit/modules/stamp/main.tf
+++ src/infra/workload/releaseunit/modules/stamp/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.38.0"
+      version = "3.46.0"
     }
     azapi = {
       source  = "Azure/azapi"
-      version = "1.2.0"
+      version = "1.4.0"
     }
   }
 }

c562d9122125a2d7cf98a74af981afc6f8337f80
Sebastian <11445087+sebader@users.noreply.github.com>
2023-03-24 19:39:48 +0100

    Make LAW quota a variable and update terraform (#898)

--- src/testing/userload-generator/infra/monitoring.tf
+++ src/testing/userload-generator/infra/monitoring.tf
@@ -4,7 +4,7 @@ resource "azurerm_log_analytics_workspace" "deployment" {
   resource_group_name = azurerm_resource_group.deployment.name
   sku                 = "PerGB2018"
   retention_in_days   = 30 # has to be between 30 and 730
-  daily_quota_gb      = 30
+  daily_quota_gb      = var.law_daily_cap_gb
 
   tags = local.default_tags
 }
@@ -15,7 +15,7 @@ resource "azurerm_application_insights" "deployment" {
   resource_group_name  = azurerm_resource_group.deployment.name
   application_type     = "web"
   workspace_id         = azurerm_log_analytics_workspace.deployment.id
-  daily_data_cap_in_gb = 30
+  daily_data_cap_in_gb = var.law_daily_cap_gb
 
   tags = local.default_tags
 }

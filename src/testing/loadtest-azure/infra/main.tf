6475632e0a29b0774e28d5a2142fe78c084ac1d1
Sebastian <11445087+sebader@users.noreply.github.com>
2023-05-08 09:12:28 +0200

    Component updates Apr-2023 (#939)
    * infra updates
    
    * fix
    
    * down
    
    * leave tf
    
    * package updates
    
    * update
    
    * update
    
    * cosmos
    
    * tf
    
    * Bump azure/azapi in /src/testing/loadtest-azure/infra (#955)
    
    Bumps [azure/azapi](https://github.com/Azure/terraform-provider-azapi) from 1.4.0 to 1.5.0.
    - [Release notes](https://github.com/Azure/terraform-provider-azapi/releases)
    - [Changelog](https://github.com/Azure/terraform-provider-azapi/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/Azure/terraform-provider-azapi/compare/v1.4.0...v1.5.0)
    
    ---
    updated-dependencies:
    - dependency-name: azure/azapi
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump grafana/grafana in /src/infra/monitoring/grafana (#946)
    
    Bumps grafana/grafana from 9.4.7 to 9.5.1.
    
    ---
    updated-dependencies:
    - dependency-name: grafana/grafana
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump @microsoft/applicationinsights-web in /src/app/AlwaysOn.UI (#940)
    
    Bumps [@microsoft/applicationinsights-web](https://github.com/microsoft/ApplicationInsights-JS) from 2.8.12 to 3.0.0.
    - [Release notes](https://github.com/microsoft/ApplicationInsights-JS/releases)
    - [Changelog](https://github.com/microsoft/ApplicationInsights-JS/blob/main/RELEASES.md)
    - [Commits](https://github.com/microsoft/ApplicationInsights-JS/compare/2.8.12...3.0.0)
    
    ---
    updated-dependencies:
    - dependency-name: "@microsoft/applicationinsights-web"
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump Microsoft.NET.Sdk.Functions (#952)
    
    Bumps [Microsoft.NET.Sdk.Functions](https://github.com/Azure/azure-functions-vs-build-sdk) from 4.1.3 to 4.2.0.
    - [Release notes](https://github.com/Azure/azure-functions-vs-build-sdk/releases)
    - [Commits](https://github.com/Azure/azure-functions-vs-build-sdk/compare/4.1.3...4.2.0)
    
    ---
    updated-dependencies:
    - dependency-name: Microsoft.NET.Sdk.Functions
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- src/testing/loadtest-azure/infra/main.tf
+++ src/testing/loadtest-azure/infra/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.50.0"
+      version = "3.55.0"
     }
     azapi = {
       source  = "azure/azapi"
-      version = "1.4.0"
+      version = "1.5.0"
     }
   }
 

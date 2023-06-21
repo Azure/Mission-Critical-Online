1151dc32fb7b3196a180b73e84b1746229c85ffc
Heyko Oelrichs <heoelri@microsoft.com>
2023-06-21 20:07:25 +0200

    rewrite missing regions

--- src/infra/workload/globalresources/modules/stamp_monitoring/prometheus.tf
+++ src/infra/workload/globalresources/modules/stamp_monitoring/prometheus.tf
@@ -1,8 +1,15 @@
+locals {
+  # regions where the creation of azure monitor workspaces is currently not supported - temporary fallback to westeurope
+  unsupported_regions = [
+    "australiaeast"
+  ]
+}
+
 resource "azapi_resource" "prometheus" {
   type      = "microsoft.monitor/accounts@2021-06-03-preview"
   name      = "${local.prefix}-${local.location_short}-prometheus"
   parent_id = var.resource_group_id
-  location  = var.location
+  location  = contains(local.unsupported_regions, var.location) ? "westeurope" : var.location
 
   response_export_values = ["*"]
 }
--- src/infra/workload/releaseunit/modules/stamp/identities.tf
+++ src/infra/workload/releaseunit/modules/stamp/identities.tf
@@ -1,27 +1,8 @@
-locals {
-  # regions where the creation of federated identity credentials is not supported on user-assigned managed identities
-  # https://learn.microsoft.com/azure/active-directory/develop/workload-identity-federation-considerations#unsupported-regions-user-assigned-managed-identities
-  unsupported_regions = [
-    "germanynorth",
-    "swedensouth",
-    "swedencentral",
-    "switzerlandwest",
-    "brazilsoutheast",
-    "eastasia",
-    "southeastasia",
-    "southafricawest",
-    "qatarcentral",
-    "australiacentral",
-    "australiacentral2",
-    "norwaywest"
-  ]
-}
-
 # managed identity used for catalogservice
 resource "azurerm_user_assigned_identity" "catalogservice" {
   # temporary workaround while the creation of federated identity credentials is not supported on user-assigned managed identities in some regions
   # https://learn.microsoft.com/azure/active-directory/develop/workload-identity-federation-considerations#unsupported-regions-user-assigned-managed-identities
-  location            = contains(local.unsupported_regions, azurerm_resource_group.stamp.location) ? "westeurope" : azurerm_resource_group.stamp.location
+  location            = azurerm_resource_group.stamp.location
   name                = "catalogservice"
   resource_group_name = azurerm_resource_group.stamp.name
 }
@@ -39,7 +20,7 @@ resource "azurerm_federated_identity_credential" "catalogservice" {
 resource "azurerm_user_assigned_identity" "healthservice" {
   # temporary workaround while the creation of federated identity credentials is not supported on user-assigned managed identities in some regions
   # https://learn.microsoft.com/azure/active-directory/develop/workload-identity-federation-considerations#unsupported-regions-user-assigned-managed-identities
-  location            = contains(local.unsupported_regions, azurerm_resource_group.stamp.location) ? "westeurope" : azurerm_resource_group.stamp.location
+  location            = azurerm_resource_group.stamp.location
   name                = "healthservice"
   resource_group_name = azurerm_resource_group.stamp.name
 }
@@ -57,7 +38,7 @@ resource "azurerm_federated_identity_credential" "healthservice" {
 resource "azurerm_user_assigned_identity" "backgroundprocessor" {
   # temporary workaround while the creation of federated identity credentials is not supported on user-assigned managed identities in some regions
   # https://learn.microsoft.com/azure/active-directory/develop/workload-identity-federation-considerations#unsupported-regions-user-assigned-managed-identities
-  location            = contains(local.unsupported_regions, azurerm_resource_group.stamp.location) ? "westeurope" : azurerm_resource_group.stamp.location
+  location            = azurerm_resource_group.stamp.location
   name                = "backgroundprocessor"
   resource_group_name = azurerm_resource_group.stamp.name
 }

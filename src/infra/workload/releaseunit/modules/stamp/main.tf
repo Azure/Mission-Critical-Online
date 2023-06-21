63ce95395056ec468d7771e5e45887d3081bcc9d
Heyko Oelrichs <heoelri@microsoft.com>
2023-06-21 06:17:45 +0200

    update provider

--- src/infra/workload/releaseunit/modules/stamp/main.tf
+++ src/infra/workload/releaseunit/modules/stamp/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.55.0"
+      version = "3.61.0"
     }
     azapi = {
       source  = "Azure/azapi"
-      version = "1.4.0"
+      version = "1.5.0"
     }
   }
 }

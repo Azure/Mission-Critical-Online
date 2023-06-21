65cbf7e0f311b3a03f041f6573da49e5d7db1def
Heyko Oelrichs <heoelri@microsoft.com>
2023-06-21 06:22:43 +0200

    add azapi to root module

--- src/infra/workload/globalresources/main.tf
+++ src/infra/workload/globalresources/main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "3.61.0"
     }
+    azapi = {
+      source  = "Azure/azapi"
+      version = "1.5.0"
+    }
   }
 
   backend "azurerm" {

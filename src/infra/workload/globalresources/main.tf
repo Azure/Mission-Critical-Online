42ef5ec78435ab2cdd80dbe088d8af322a66f507
Sebastian <11445087+sebader@users.noreply.github.com>
2023-07-03 17:13:18 +0200

    Component updates July-2023 (#1015)
    * Bump Microsoft.Azure.WebJobs.Extensions.DurableTask
    
    Bumps [Microsoft.Azure.WebJobs.Extensions.DurableTask](https://github.com/Azure/azure-functions-durable-extension) from 2.9.5 to 2.9.6.
    - [Release notes](https://github.com/Azure/azure-functions-durable-extension/releases)
    - [Changelog](https://github.com/Azure/azure-functions-durable-extension/blob/dev/azure-pipelines-analyzer-release.yml)
    - [Commits](https://github.com/Azure/azure-functions-durable-extension/compare/v2.9.5...v2.9.6)
    
    ---
    updated-dependencies:
    - dependency-name: Microsoft.Azure.WebJobs.Extensions.DurableTask
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * nuget
    
    * npm
    
    * tf
    
    * user test
    
    * tf
    
    * Update main.tf
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- src/infra/workload/globalresources/main.tf
+++ src/infra/workload/globalresources/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.61.0"
+      version = "3.63.0"
     }
     azapi = {
       source  = "Azure/azapi"
-      version = "1.5.0"
+      version = "1.7.0"
     }
   }
 

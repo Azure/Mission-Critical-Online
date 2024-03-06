818792170ae49bb4c5170653749aec06787a4b22
Sebastian <11445087+sebader@users.noreply.github.com>
2024-03-06 18:16:22 +0100

    Component updates March-2024 (#1326)
    update

--- src/infra/workload/releaseunit/modules/stamp/main.tf
+++ src/infra/workload/releaseunit/modules/stamp/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.90.0"
+      version = "3.94.0"
     }
     azapi = {
       source  = "Azure/azapi"
-      version = "1.12.0"
+      version = "1.12.1"
     }
   }
 }
